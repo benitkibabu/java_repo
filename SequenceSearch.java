bb93dba7f1905e8f3b76a0ffd8fb85208682bc80
benitkibabu <benit@commodityvectors.com>
2013-11-12 15:52:49 +0000

    Create SequenceSearch.java

---
+++ SequenceSearch.java
@@ -0,0 +1,26 @@
+import java.util.io
+
+public class MyArrayList <ElemType> extends ArrayList<ElemType>{
+
+  public int unorderSearc(ElemType key){
+    int k=0;
+    for(k =1; k<size(); k++){
+      if(((Comparable)get(k)).compareTo((Comparable)key) ==0){
+        return k;
+      }
+    }
+    return -1;
+  }
+  
+  public int orderSearch(ElemType key){
+    int k=0;
+    while(((Comparable)get(key)).compareTo((Comparable)key) <0 && k < size()){
+      k= k+ 1;
+    }
+    if(((Comparable)get(k)).compareTo((Comparable)key) ==0){
+      return k;
+    }
+    else
+      return -1;
+  }
+}
